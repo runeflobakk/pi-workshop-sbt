522d5a2461039d703c121f875dfc8be4fdb4c90b
Rune Flobakk <flobakk@gmail.com>
2013-04-02 20:39:31 +0200

    Divide and conquer

---
+++ src/main/scala/no/bekk/piworkshop/Leds.scala
@@ -0,0 +1,29 @@
+package no.bekk.piworkshop
+
+import com.pi4j.io.gpio.GpioFactory
+import com.pi4j.io.gpio.PinState.{LOW => OFF}
+import com.pi4j.io.gpio.PinState.{LOW => ON}
+import com.pi4j.io.gpio.RaspiPin
+import com.pi4j.io.gpio.GpioPinDigitalOutput
+
+object Gpio {
+  
+  implicit class Led(gpioController: GpioPinDigitalOutput) {
+    def on = gpioController.setState(ON);
+    def off = gpioController.setState(OFF);
+  }
+  
+  private val gpio = GpioFactory.getInstance();
+  
+  lazy val leds = List(
+      gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "gpio0", OFF),
+      gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "gpio1", OFF),
+      gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, "gpio2", OFF),
+      gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, "gpio3", OFF),
+      gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "gpio4", OFF),
+      gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05, "gpio5", OFF),
+      gpio.provisionDigitalOutputPin(RaspiPin.GPIO_06, "gpio6", OFF),
+      gpio.provisionDigitalOutputPin(RaspiPin.GPIO_07, "gpio7", OFF)
+  )
+}
+
--- src/main/scala/no/bekk/piworkshop/ScalaPi.scala
+++ src/main/scala/no/bekk/piworkshop/ScalaPi.scala
@@ -6,27 +6,11 @@ import com.pi4j.io.gpio.PinState.{LOW => OFF}
 import com.pi4j.io.gpio.PinState.{HIGH => ON}
 import com.pi4j.io.gpio.GpioController
 import com.pi4j.io.gpio.GpioPinDigitalOutput
+import Gpio.leds
+
 
 object ScalaPi {
 
-  private val gpio = GpioFactory.getInstance();
-  
-  private val leds = List(
-      gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "gpio0", OFF),
-      gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "gpio1", OFF),
-      gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, "gpio2", OFF),
-      gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, "gpio3", OFF),
-      gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "gpio4", OFF),
-      gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05, "gpio5", OFF),
-      gpio.provisionDigitalOutputPin(RaspiPin.GPIO_06, "gpio6", OFF),
-      gpio.provisionDigitalOutputPin(RaspiPin.GPIO_07, "gpio7", OFF)
-  )
-  
-  implicit class Led(gpioController: GpioPinDigitalOutput) {
-    def on = gpioController.setState(ON);
-    def off = gpioController.setState(OFF);
-  }
-  
   def main(args: Array[String]) {
         
         leds foreach(_.on)
@@ -36,5 +20,10 @@ object ScalaPi {
         
         println("and off...");
         leds foreach(_.off)
+  }
+  
+  implicit class Led(gpioController: GpioPinDigitalOutput) {
+    def on = gpioController.setState(ON);
+    def off = gpioController.setState(OFF);
   }
 }
